ec87cb67c9af7e6975202b36b26ed9740a5dff37
Jon Hadfield <jon@lessknown.co.uk>
2015-05-19 20:52:30 +0100

    add validation for hostnames.

--- hosts/utils.py
+++ hosts/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import socket
+import re
 
 def is_ipv4(entry):
     """ Checks if a string is a valid ipv4 address. """
@@ -16,3 +17,13 @@ def is_ipv6(entry):
             return True
     except socket.error:
         return False
+
+def are_valid_hostnames(hostname_list):
+    """ Checks if all provided hostnames are valid. """
+    for entry in hostname_list:
+        if len(entry) > 255:
+            return False
+        allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+        if not all(allowed.match(x) for x in entry.split(".")):
+            return False
+    return True
