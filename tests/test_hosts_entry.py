99636e0ebe4a93448db09ff361be97194d13c5bd
Jon Hadfield <jon@lessknown.co.uk>
2015-05-19 20:55:00 +0100

    remove unused import.

--- tests/test_hosts_entry.py
+++ tests/test_hosts_entry.py
@@ -2,12 +2,12 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'hosts')))
-from hosts import Hosts, HostsEntry
-
+from hosts import HostsEntry
 
 def test_create_ipv4_instance():
-    ''' add an ipv4 type entry '''
+    """ add an ipv4 type entry """
     hosts_entry = HostsEntry(entry_type='ipv4', address='1.2.3.4', names=['example.com', 'example'])
     assert hosts_entry.entry_type == 'ipv4'
     assert hosts_entry.address == '1.2.3.4'
     assert hosts_entry.names == ['example.com', 'example']
+
