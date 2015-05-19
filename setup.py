f464f1acf61446c38aabead2d762dfe6563ee7c7
Jon Hadfield <jon@lessknown.co.uk>
2015-05-19 15:52:52 +0100

    bump version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = "0.1.0"
+version = "0.1.1"
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
