a5976a5fc7be11c017cde401fb68b425ee038203
Jon Hadfield <jon@lessknown.co.uk>
2015-05-22 17:52:21 +0100

    pep8

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,11 +4,13 @@ sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), '..', 'hosts')
 from os import path
 from hosts.utils import is_ipv4, is_ipv6, valid_hostnames
 
+
 def test_ipv4_validation_success():
     """
     Test function returns True if valid IPv4 address is specified
     """
     assert is_ipv4('8.8.8.8')
+
 
 def test_ipv4_validation_failure():
     """
@@ -16,11 +18,13 @@ def test_ipv4_validation_failure():
     """
     assert not is_ipv4('256.8.8.8')
 
+
 def test_ipv6_validation_success():
     """
     Test function returns True if valid IPv4 address is specified
     """
     assert is_ipv6('2001:db8::ff00:42:8329')
+
 
 def test_ipv6_validation_failure():
     """
@@ -28,11 +32,13 @@ def test_ipv6_validation_failure():
     """
     assert not is_ipv6('2001::0234:C1ab::A0:aabc:003F')
 
+
 def test_hostname_validation_success():
     """
     Test function returns True if valid hostnames are specified
     """
     assert valid_hostnames(['example.com', 'example'])
+
 
 def test_hostname_validation_failure_too_long():
     """
@@ -41,9 +47,9 @@ def test_hostname_validation_failure_too_long():
     long_hostname = 'x' * 256
     assert not valid_hostnames(['example.com', long_hostname])
 
+
 def test_hostname_validation_failure_with_leading_hyphen():
     """
     Test function returns False if a hostname with a leading hyphen is specified
     """
     assert not valid_hostnames(['example.com', '-example'])
-
