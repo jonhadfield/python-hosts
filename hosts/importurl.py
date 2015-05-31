9f43770c2055c6a9d9be34a65954db101f1dbc9f
Jon Hadfield <jon@lessknown.co.uk>
2015-05-31 20:04:43 +0100

    add test for profiling.

---
+++ hosts/importurl.py
@@ -0,0 +1,6 @@
+from hosts import Hosts
+hosts = Hosts(path='/tmp/hosts')
+#hosts.import_url('https://dl.dropboxusercontent.com/u/167103/hosts')
+hosts.import_url('http://www.winhelp2002.mvps.org/hosts.txt')
+#hosts.import_url('https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts')
+#hosts.import_url('https://dl.dropboxusercontent.com/u/167103/hosts2')
