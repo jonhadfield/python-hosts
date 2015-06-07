a1a6fa3ab6ce4ff972d2f775e22fad573c03fe60
Jon Hadfield <jon@lessknown.co.uk>
2015-06-07 19:14:47 +0100

    another try at imports.

--- python_hosts/__init__.py
+++ python_hosts/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-import hosts
+from hosts import Hosts, HostsEntry
+from utils import is_readable, is_ipv4, is_ipv6, is_writeable, valid_hostnames
 import exception
-import utils
