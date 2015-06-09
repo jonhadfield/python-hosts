dfb5fa35673c2a64a07556af5f91099e9bb08c99
Jon Hadfield <jon@lessknown.co.uk>
2015-06-09 22:12:27 +0100

    add docs.

---
+++ python_hosts/docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to python-hosts&#8217;s documentation!"],titleterms:{document:0,host:0,indic:0,python:0,tabl:0,welcom:0}})
