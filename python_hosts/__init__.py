304eaa383ac5b83574fe6dd9e2acd2d2e601891a
Jon Hadfield <jon@lessknown.co.uk>
2015-08-10 17:57:09 +0100

    make docstrings more pretty for sphinx.

--- python_hosts/__init__.py
+++ python_hosts/__init__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-This package contains all of the modules utilised
-by the python-hosts library.
-hosts: Contains the Hosts and HostsEntry classes that represent instances
- of a hosts file and it's individual lines/entries
-utils: Contains helper functions to check the available operations on a hosts
- file and the validity of a hosts file entry
-exception: Contains the custom exceptions that are raised in the event
- of an error in processing a hosts file and its entries
+This package contains all of the modules utilised by the python-hosts library.
+
+hosts: Contains the Hosts and HostsEntry classes that represent instances of a hosts file and it's individual lines/entries
+
+utils: Contains helper functions to check the available operations on a hosts file and the validity of a hosts file entry
+
+exception: Contains the custom exceptions that are raised in the event of an error in processing a hosts file and its entries
 """
 from python_hosts.hosts import Hosts, HostsEntry
 from python_hosts.utils import (is_readable, is_ipv4, is_ipv6,
