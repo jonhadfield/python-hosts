67395e8f513def791d345eff3f3fad6c01fc8924
Jon Hadfield <jon@lessknown.co.uk>
2015-08-11 12:26:38 +0100

    simplify write checks.

--- python_hosts/exception.py
+++ python_hosts/exception.py
@@ -9,37 +9,38 @@ All exceptions used in the hosts code base are defined here.
 
 
 class HostsException(Exception):
-    """
-    Base exception class. All Hosts-specific exceptions should subclass
+    """ Base exception class. All Hosts-specific exceptions should subclass
     this class.
     """
     pass
 
+class UnableToWriteHosts(HostsException):
+    """ Raised when a Hosts file cannot be written. """
+    pass
 
 class HostsEntryException(Exception):
-    """
-    Base exception class. All HostsEntry-specific exceptions should subclass
-    this class.
+    """ Base exception class. All HostsEntry-specific exceptions should
+    subclass this class.
     """
     pass
 
 
 class InvalidIPv4Address(HostsEntryException):
-    """
-    Raised when a HostsEntry is defined as type 'ipv4' but with an invalid address
+    """ Raised when a HostsEntry is defined as type 'ipv4' but with an
+    invalid address.
     """
     pass
 
 
 class InvalidIPv6Address(HostsEntryException):
-    """
-    Raised when a HostsEntry is defined as type 'ipv6' but with an invalid address
+    """ Raised when a HostsEntry is defined as type 'ipv6' but
+    with an invalid address.
     """
     pass
 
 
 class InvalidComment(HostsEntryException):
-    """
-    Raised when a HostsEntry is defined as type 'comment' but with an invalid comment
+    """ Raised when a HostsEntry is defined as type 'comment' but with an
+    invalid comment
     """
     pass
