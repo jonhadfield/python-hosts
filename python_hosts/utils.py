eee9c0f8939dd592b6af09ba0f7d69b61e745ec0
Jon Hadfield <jon@lessknown.co.uk>
2015-08-11 12:27:35 +0100

    remove factored out method.

--- python_hosts/utils.py
+++ python_hosts/utils.py
@@ -57,13 +57,3 @@ def is_readable(path=None):
     if os.path.isfile(path) and os.access(path, os.R_OK):
         return True
     return False
-
-
-def is_writeable(path=None):
-    """
-    Test if the supplied filesystem path can be written to
-    :param path: A filesystem path
-    :return: True if the path is a file that can be written. Otherwise, False
-    """
-    if os.path.isfile(path) and os.access(path, os.W_OK):
-        return True
