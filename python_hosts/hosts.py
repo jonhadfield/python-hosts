9741ccb1174228307700b16e3b355e1d22dc7c70
Jon Hadfield <jon@lessknown.co.uk>
2015-08-11 12:11:36 +0100

    remove useless variable assignment.

--- python_hosts/hosts.py
+++ python_hosts/hosts.py
@@ -304,8 +304,7 @@ class Hosts(object):
         :return: A list with original duplicates removed
         """
         seen = set()
-        seen_add = seen.add
-        return (x for x in seq if not (x in seen or seen_add(x)))
+        return (x for x in seq if not (x in seen or seen.add(x)))
 
     def add(self, entries=None, force=False):
         """
