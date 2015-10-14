b1ca8a6a29d9d7dd0af173c20f3fe3dd56b3e226
Jon Hadfield <jon@lessknown.co.uk>
2015-10-14 13:21:47 +0100

    let chmod mode work with both python 2 and 3.

--- tests/test_hosts.py
+++ tests/test_hosts.py
@@ -57,8 +57,8 @@ def test_exception_raised_when_unable_to_write_hosts(tmpdir):
     hosts_file.write("127.0.0.1\tlocalhost\n")
     hosts = Hosts(path=hosts_file.strpath)
     mode = int('0440', 8)
-    if sys.version_info[0] == 3:
-        mode = '0o440'
+    # if sys.version_info[0] == 3:
+    #    mode = 0o440
     os.chmod(hosts_file.strpath, mode)
     new_entry = HostsEntry(entry_type='ipv4', address='123.123.123.123', names=['test.example.com'])
     hosts.add(entries=[new_entry])
