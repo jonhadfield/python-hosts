c1403cc8beac2a9142a21c2de555eb9e5e090f9b
Jon Hadfield <jon@lessknown.co.uk>
2016-03-20 21:55:04 +0000

    Revert "refactor: remove unused imports."
    This reverts commit ceb37d26a48d7d56b3d0ded0376eb9498ce7ef6f.

--- python_hosts/__init__.py
+++ python_hosts/__init__.py
@@ -8,7 +8,8 @@ utils: Contains helper functions to check the available operations on a hosts fi
 
 exception: Contains the custom exceptions that are raised in the event of an error in processing a hosts file and its entries
 """
+from python_hosts.hosts import Hosts, HostsEntry
+from python_hosts.utils import is_readable, is_ipv4, is_ipv6, valid_hostnames
 from python_hosts.exception import (HostsException, HostsEntryException,
-                                    InvalidIPv4Address, InvalidComment)
-from python_hosts.hosts import Hosts
-from python_hosts.utils import is_readable, is_ipv4, is_ipv6
+                                    InvalidIPv4Address, InvalidIPv6Address,
+                                    InvalidComment)
