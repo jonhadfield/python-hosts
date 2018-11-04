ef5579c90149490aa0c1b1206d17f2a5f4abe911
Jon Hadfield <jon@lessknown.co.uk>
2018-11-04 13:59:14 +0000

    add requirements.txt for pypi.

--- python_hosts/__init__.py
+++ python_hosts/__init__.py
@@ -8,9 +8,9 @@ utils: Contains helper functions to check the available operations on a hosts fi
 
 exception: Contains the custom exceptions that are raised in the event of an error in processing a hosts file and its entries
 """
-name = "python_hosts"
 from python_hosts.hosts import Hosts, HostsEntry
 from python_hosts.utils import is_readable, is_ipv4, is_ipv6, valid_hostnames
 from python_hosts.exception import (HostsException, HostsEntryException,
                                     InvalidIPv4Address, InvalidIPv6Address,
                                     InvalidComment)
+name = "python_hosts"
