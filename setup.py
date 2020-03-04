2d2ca2af1cc38e434b9875da44face4a589a9df3
Jon Hadfield <jon@lessknown.co.uk>
2020-03-04 18:01:03 +0000

    bump release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
-version = "0.4.7"
+version = "1.0.0"
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload -r pypi')
